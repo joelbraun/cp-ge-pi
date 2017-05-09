85fedc81d929d91fee3913ead1461172a42af3c6
Brady Aiello <bbaiello@calpoly.edu>
2017-05-09 16:35:13 -0700

    Added new pixel_selector that will select pixels from any image, not the pixel of a display.
    Signed-off-by: Brady Aiello <bbaiello@calpoly.edu>

--- pixel_selector/pixel_selector.py
+++ pixel_selector/pixel_selector.py
@@ -1,36 +1,55 @@
-import sys
-import pymouse
-import re
-from pymouse import PyMouseEvent
+from Tkinter import *
+from tkFileDialog import askopenfilename
+from PIL import Image, ImageTk
 
 
-class Clicker(PyMouseEvent):
-    def __init__(self, num_parking_spots):
-        PyMouseEvent.__init__(self)
+class Clicker():
+    def __init__(self, num_spots):
         self.point_ctr = 0
-        self.line_ctr = 0
-        self.num_parking_spots = num_parking_spots
-        self.mouse = pymouse.PyMouse()
-        sys.stdout.write(str(self.line_ctr))
+        self.num_parking_spots = num_spots
+        self.id_current_spot = 0
 
+    def click(self, event):
+        if self.point_ctr == self.id_current_spot == 0:
+            print(str(self.id_current_spot)),
+            # outputting x and y coords to console
+        print(str(event.x) + " " + str(event.y)),
+        self.point_ctr = (self.point_ctr + 1) % 4
 
-    def click(self, x, y, button, press):
-        if button == 1:
-            if press:
-                pos_str = re.sub('[(,)]', '', str(self.mouse.position()))
-                sys.stdout.write(" " + pos_str)
-                self.point_ctr = (self.point_ctr + 1) % 4
-                if self.point_ctr == 0:
-                    sys.stdout.write('\n')
-                    self.line_ctr += 1
-                    if self.line_ctr == self.num_parking_spots:
-                        exit(0)
-                    sys.stdout.write(str(self.line_ctr))
+        if self.point_ctr == 0:
+            print("")
+            self.id_current_spot += 1
+        if self.id_current_spot == self.num_parking_spots and self.point_ctr == 0:
+            exit(0)
 
-        else:  # Exit if any other mouse button used
-            self.stop()
+if __name__ == "__main__":
+    root = Tk()
+    #setting up a tkinter canvas with scrollbars
+    frame = Frame(root, bd=2, relief=SUNKEN)
+    frame.grid_rowconfigure(0, weight=1)
+    frame.grid_columnconfigure(0, weight=1)
+    xscroll = Scrollbar(frame, orient=HORIZONTAL)
+    xscroll.grid(row=1, column=0, sticky=E+W)
+    yscroll = Scrollbar(frame)
+    yscroll.grid(row=0, column=1, sticky=N+S)
+    canvas = Canvas(frame, bd=0, xscrollcommand=xscroll.set, yscrollcommand=yscroll.set)
+    canvas.grid(row=0, column=0, sticky=N+S+E+W)
+    xscroll.config(command=canvas.xview)
+    yscroll.config(command=canvas.yview)
+    frame.pack(fill=BOTH,expand=1)
 
-#C = Clicker(int(sys.argv[1]))
-C = Clicker(2)
-C.run()
+    #adding the image
+    File = askopenfilename(parent=root, initialdir="C:/",title='Choose an image.')
+    img = ImageTk.PhotoImage(Image.open(File))
+    canvas.create_image(0,0,image=img,anchor="nw")
+    canvas.config(scrollregion=canvas.bbox(ALL))
+    clicker = Clicker(int(sys.argv[1]))
 
+    #function to be called when mouse is clicked
+    def printcoords(event):
+        clicker.click(event)
+
+    #mouseclick event
+    canvas.bind("<Button 1>", printcoords)
+
+    root.mainloop()
